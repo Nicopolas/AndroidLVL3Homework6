e48656827aacff13e7187535658e92003f936b0f
nicopolas <slepdog@gmail.com>
2018-07-10 21:16:38 +0300

    add stetho

--- app/src/main/java/zakharov/nikolay/com/androidlvl3homework6/MainApp.java
+++ app/src/main/java/zakharov/nikolay/com/androidlvl3homework6/MainApp.java
@@ -3,6 +3,7 @@ package zakharov.nikolay.com.androidlvl3homework6;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
 import com.orm.SugarContext;
 
 import io.realm.Realm;
@@ -20,6 +21,30 @@ public class MainApp extends Application {
         Realm.init(this);
         RealmConfiguration configuration = new RealmConfiguration.Builder().build();
         Realm.setDefaultConfiguration(configuration);
+
+        stechoInit();
+    }
+
+    private void stechoInit() {
+        // Create an InitializerBuilder
+        Stetho.InitializerBuilder initializerBuilder =
+                Stetho.newInitializerBuilder(this);
+
+        // Enable Chrome DevTools
+        initializerBuilder.enableWebKitInspector(
+                Stetho.defaultInspectorModulesProvider(this)
+        );
+
+        // Enable command line interface
+        initializerBuilder.enableDumpapp(
+                Stetho.defaultDumperPluginsProvider(this)
+        );
+
+        // Use the InitializerBuilder to generate an Initializer
+        Stetho.Initializer initializer = initializerBuilder.build();
+
+        // Initialize Stetho with the Initializer
+        Stetho.initialize(initializer);
     }
 }
 
